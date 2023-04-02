5716bcf0c9f649dd3ef6dab5652dd15c88c0f692
xzpnt <129638449+xzpnt@users.noreply.github.com>
2023-04-02 05:00:24 +0200

    Add files via upload

---
+++ Escaneo.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import nmap
+
+print(" __                 ")
+print("'. \                ")
+print(" '- \               ")
+print("  / /_         .---.")
+print(" / | \\,.\/--.//    )")
+print(" |  \//        )/  / ")
+print("  \  ' ^ ^    /    )____.----..  6")
+print("   '.____.    .___/            \._)")
+print("      .\/.                      )")
+print("       '\                       /")
+print("       _/ \/    ).        )    (")
+print("      /#  .!    |        /\    /")
+print("      \  C// #  /'-----''/ #  /")
+print("   .   'C/ |    |    |   |    |mrf  ,")
+print("   \), .. .'OOO-'. ..'OOO'OOO-'. ..\(,")
+
+
+ip = input("[+] Introduce la dirección IP objetivo: ")
+
+nm = nmap.PortScanner()
+
+results = nm.scan(hosts=ip, arguments="-sS -O") 
+print("Host : %s" % ip)
+print("State : %s" % nm[ip].state())
+
+if 'osmatch' in nm[ip]:
+    for osmatch in nm[ip]['osmatch']:
+        print('[+] Sistema Operativo : %s' % osmatch['name'])
+
+for proto in nm[ip].all_protocols():
+    print('Protocol : %s' % proto)
+    lport = nm[ip][proto].keys()
+    sorted(lport)
+    for port in lport:
+        print('port : %s\tstate: %s' % (port, nm[ip][proto][port]['state']))
+        print("Nombre del servicio: %s" % nm[ip][proto][port]['name'])
+        print("Producto: %s" % nm[ip][proto][port]['product'])
+        print("Version: %s" % nm[ip][proto][port]['version'])
+        print("OS: %s" % nm[ip]['osmatch'][0]['name'])
+
+
